019ccba0ac09f3d4fa916d174fbddba3a9fdc458
ingecraft <ingenious.craftsman@gmail.com>
2020-04-08 21:01:56 +0300

    L87: Signing up and writing Users to a database

--- section5/user.py
+++ section5/user.py
@@ -1,5 +1,68 @@
+import sqlite3
+from flask_restful import Resource, reqparse
+
 class User:
     def __init__(self, _id, username, password):
         self.id = _id
         self.username = username
         self.password = password
+
+    @classmethod
+    def find_by_username(cls, username):
+        connection = sqlite3.connect('data.db')
+        cursor = connection.cursor()
+
+        query = "SELECT * FROM users WHERE username=?"
+        result = cursor.execute(query, (username,))
+        row = result.fetchone()
+        if row:
+            user = cls(*row)
+        else:
+            user = None
+
+        connection.close()
+        return user
+
+
+    @classmethod
+    def find_by_id(cls, _id):
+        connection = sqlite3.connect('data.db')
+        cursor = connection.cursor()
+
+        query = "SELECT * FROM users WHERE id=?"
+        result = cursor.execute(query, (_id,))
+        row = result.fetchone()
+        if row:
+            user = cls(*row)
+        else:
+            user = None
+
+        connection.close()
+        return user
+
+
+class UserRegister(Resource):
+    def post(self):
+        connection = sqlite3.connect('data.db')
+        cursor = connection.cursor()
+
+        parser = reqparse.RequestParser()
+        parser.add_argument('username',
+                            type=str,
+                            required=True,
+                            help="The username field cannot be left blank"
+                           )
+        parser.add_argument('password',
+                            type=str,
+                            required=True,
+                            help="The password field cannot be left blank"
+                           )
+        data = parser.parse_args()
+        query = "INSERT INTO users VALUES (NULL, ?, ?)"
+        cursor.execute(query, (data['username'], data['password']))
+
+        connection.commit()
+        connection.close()
+
+        return {"message": "User created succesfully."}, 201
+
