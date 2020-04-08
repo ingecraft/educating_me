50fad31de8546f1bc4cbabb897d420daccc71474
ingecraft <ingenious.craftsman@gmail.com>
2020-04-08 13:05:25 +0300

    L77: DELETE to delete items

--- section4/app.py
+++ section4/app.py
@@ -20,7 +20,6 @@ class Item(Resource):
         item = next(filter(lambda x: x['name'] == name, items), None)
         return {'item': item}, 200 if item else 404
 
-
     def post(self, name):
         if next(filter(lambda x: x['name'] == name, items), None):
             return {'message': "An item with name '{}' already exists" \
@@ -30,6 +29,11 @@ class Item(Resource):
         item = {'name': name, 'price': data['price']}
         items.append(item)
         return item, 201
+
+    def delete(self, name):
+        global items
+        items = list(filter(lambda x: x['name'] != name, items))
+        return {'message': 'Item deleted'}
 
 
 class ItemList(Resource):
