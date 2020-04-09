a321cd99e4699801af7dadfdcf81a89ad6b16c91
ingecraft <ingenious.craftsman@gmail.com>
2020-04-09 15:40:59 +0300

    L89: Retrieving our Item resources from a database

--- section5/app.py
+++ section5/app.py
@@ -11,7 +11,7 @@ app = Flask(__name__)
 app.secret_key = 'bandit'
 api = Api(app)
 
-jwt = JWT(app, authenticate, identity) # /auth
+jwt = JWT(app, authenticate, identity)
 
 items = []
 
--- section5/user.py
+++ section5/user.py
@@ -1,6 +1,7 @@
 import sqlite3
 from flask_restful import Resource, reqparse
 
+
 class User:
     def __init__(self, _id, username, password):
         self.id = _id
@@ -23,7 +24,6 @@ class User:
         connection.close()
         return user
 
-
     @classmethod
     def find_by_id(cls, _id):
         connection = sqlite3.connect('data.db')
@@ -42,22 +42,26 @@ class User:
 
 
 class UserRegister(Resource):
+    parser = reqparse.RequestParser()
+    parser.add_argument('username',
+                        type=str,
+                        required=True,
+                        help="The username field cannot be left blank")
+
+    parser.add_argument('password',
+                        type=str,
+                        required=True,
+                        help="The password field cannot be left blank")
+
     def post(self):
+        data = UserRegister.parser.parse_args()
+
+        if User.find_by_username(data['username']):
+            return {"message": "User already exists."}, 400
+
         connection = sqlite3.connect('data.db')
         cursor = connection.cursor()
 
-        parser = reqparse.RequestParser()
-        parser.add_argument('username',
-                            type=str,
-                            required=True,
-                            help="The username field cannot be left blank"
-                           )
-        parser.add_argument('password',
-                            type=str,
-                            required=True,
-                            help="The password field cannot be left blank"
-                           )
-        data = parser.parse_args()
         query = "INSERT INTO users VALUES (NULL, ?, ?)"
         cursor.execute(query, (data['username'], data['password']))
 
@@ -65,4 +69,3 @@ class UserRegister(Resource):
         connection.close()
 
         return {"message": "User created succesfully."}, 201
-
