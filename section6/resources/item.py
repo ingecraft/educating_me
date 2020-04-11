5f39e591bb870f612367091a7607fa9b8d953934
ingecraft <ingenious.craftsman@gmail.com>
2020-04-11 21:52:14 +0300

    Implementing the ItemModel using SQLAlchemy

--- section6/resources/item.py
+++ section6/resources/item.py
@@ -28,7 +28,7 @@ class Item(Resource):
         item = ItemModel(name, data['price'])
 
         try:
-            item.insert()
+            item.save_to_db()
         except Exception:
             return {'message': 'An error occured inserting an item'}, 500
             # 500 Internal server error
@@ -36,14 +36,9 @@ class Item(Resource):
         return item.json(), 201
 
     def delete(self, name):
-        connection = sqlite3.connect('data.db')
-        cursor = connection.cursor()
-
-        query = "DELETE FROM items WHERE name=?"
-        cursor.execute(query, (name,))
-
-        connection.commit()
-        connection.close()
+        item = ItemModel.find_by_name(name)
+        if item:
+            item.delete_from_db()
 
         return {'message': 'Item deleted'}, 200
 
@@ -51,20 +46,16 @@ class Item(Resource):
         data = Item.parser.parse_args()
 
         item = ItemModel.find_by_name(name)
-        updated_item = ItemModel(name, data['price']) 
 
         if item is None:
-            try:
-                item.insert()
-            except Exception:
-                return {'message': 'An error occured inserting item'}, 500
+            item = ItemModel(name, data['price'])
         else:
-            try:
-                updated_item.update()
-            except Exception:
-                return {'message': 'An error occured inserting updating item'},
-                500
-        return updated_item.json()
+            item.price = data['price']
+ 
+        item.save_to_db()
+
+        return item.json()
+
 
 
 class ItemList(Resource):
