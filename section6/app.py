a573203de967a97a47b5f5dd181e02fbb3e61158
ingecraft <ingenious.craftsman@gmail.com>
2020-04-12 20:31:54 +0300

    L:111. Creating the Store Resource:

--- section6/app.py
+++ section6/app.py
@@ -5,6 +5,7 @@ from flask_jwt import JWT
 from security import authenticate, identity
 from resources.user import UserRegister
 from resources.item import Item, ItemList
+from resources.store import Store, StoreList
 from db import db
 
 app = Flask(__name__)
@@ -25,6 +26,8 @@ jwt = JWT(app, authenticate, identity)
 api.add_resource(Item, '/item/<string:name>')
 api.add_resource(ItemList, '/items')
 api.add_resource(UserRegister, '/register')
+api.add_resource(Store, '/store/<string:name>')
+api.add_resource(StoreList, '/stores')
 
 if __name__ == "__main__":
     app.run(port=5000, debug=True)
