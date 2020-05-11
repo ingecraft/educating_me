992b3488e0211a99a40e24a05b34c66d6d08a207
ingecraft <ingenious.craftsman@gmail.com>
2020-05-11 18:56:13 +0300

    Finished exercise 2, chapter4

--- js/eloquent_javascript/chapter4/exercises.js
+++ js/eloquent_javascript/chapter4/exercises.js
@@ -9,6 +9,8 @@ function range(start, end) {
 	return array;
 }
 
+console.log(range(1,10));
+
 function sum(array) {
 	sum = 0;
 	for (const number of array) {
@@ -18,7 +20,6 @@ function sum(array) {
 	return sum;
 }
 
-console.log(range(1,10));
 console.log(sum(range(1,10)));
 
 console.log('\n--- Exercise 2---\n');
@@ -27,10 +28,22 @@ function reverseArray(array) {
 	reversed = [];
 	for (let i=array.length-1; i >= 0; i--) {
 		reversed.push(array[i]);
-	}
+	};
 
 	return reversed;
 }
 
 console.log(reverseArray(["A", "B", "C"]));
 
+function reverseArrayInPlace(array) {
+  for (let i = 0; i < Math.floor(array.length / 2); i++) {
+    let old = array[i];
+    array[i] = array[array.length - 1 - i];
+    array[array.length - 1 - i] = old;
+  }
+  return array;
+}
+
+let arrayValue = [1, 2, 3, 4, 5];
+reverseArrayInPlace(arrayValue);
+console.log(arrayValue);
