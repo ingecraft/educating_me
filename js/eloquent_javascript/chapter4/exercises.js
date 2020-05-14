e69028e33268576c9c26956b19c3eb1a5a2d8540
ingecraft <ingenious.craftsman@gmail.com>
2020-05-14 13:16:11 +0300

    Chapter 4, exercise 3

--- js/eloquent_javascript/chapter4/exercises.js
+++ js/eloquent_javascript/chapter4/exercises.js
@@ -47,3 +47,51 @@ function reverseArrayInPlace(array) {
 let arrayValue = [1, 2, 3, 4, 5];
 reverseArrayInPlace(arrayValue);
 console.log(arrayValue);
+
+console.log('\n--- Exercise 3---\n');
+
+function arrayToList(array) {
+	list = {};
+	for (let i=array.length-1; i >= 0; i--) {
+	  if (i == array.length-1) {
+			list = {'value': array[i], rest: null};
+		} else {
+			list = {'value': array[i], rest: list};
+		}
+	}
+
+	return list;
+}
+
+function listToArray(list) {
+	let array = [];
+	for (let node = list; node; node = node.rest) {
+		array.push(node.value);
+	}
+	return array;
+}
+	
+function prepend(value, list) {
+	return {value, 'rest': list};
+}
+
+function nth(list, index) {
+	if (index == 0) {
+		return list.value;
+	}
+	if (list) {
+		index -= 1;
+		return nth(list.rest, index);
+	} else {
+		return undefined;
+	}
+}
+
+console.log(arrayToList([10, 20]));
+// → {value: 10, rest: {value: 20, rest: null}}
+console.log(listToArray(arrayToList([10, 20, 30])));
+// → [10, 20, 30]
+console.log(prepend(10, prepend(20, null)));
+// → {value: 10, rest: {value: 20, rest: null}}
+console.log(nth(arrayToList([10, 20, 30]), 2));
+// → 20
