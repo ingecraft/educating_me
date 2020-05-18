6d1276bc750d3a5e5bd0dc7880c1136c7ade9e91
ingecraft <ingenious.craftsman@gmail.com>
2020-05-18 19:22:18 +0300

    Finished exercise 2

--- js/eloquent_javascript/chapter5/exercises.js
+++ js/eloquent_javascript/chapter5/exercises.js
@@ -15,9 +15,25 @@ function flatten(arrays) {
 	return arrays.reduce((flat, current) => flat.concat(current), []);
 }
 
+
+console.log('--- Exercise 1 ---\n');
+
 let arrays = [[1, 2, 3], [4, 5], [6]];
 // Your code here.
 console.log(flatten(arrays));
 // → [1, 2, 3, 4, 5, 6]`
 
 console.log('\n--- Exercise 2---\n');
+
+function loop(value, test, update, body) {
+  if (test(value)){
+    body(value);
+    loop(update(value), test, update, body);
+  }
+}
+
+loop(3, n => n > 0, n => n - 1, console.log);
+// → 3
+// → 2
+// → 1
+
